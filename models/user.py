2e76327bd753c39207d45dbf2603681e175f5827
Abualiy <87162921+Abualiy@users.noreply.github.com>
2023-03-22 12:10:33 +0300

    hotFix

--- models/user.py
+++ models/user.py
@@ -5,7 +5,18 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = = Column(String(128), nullable=False)
+        frist_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                              cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
