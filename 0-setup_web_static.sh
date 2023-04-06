99a32cfa8f2e6d36d45cece65a892a61fda221bd
Yosinan <ymymym4634@gmial.com>
2023-04-06 12:45:02 -0700

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,9 +2,10 @@
 # Preparing my web servers - for web_static deployment
 sudo apt-get update
 sudo apt-get install nginx -y
-suod mkdir -p /data/web_static/shared /data/web_static/releases/test
-echo "This is a sample html file" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
