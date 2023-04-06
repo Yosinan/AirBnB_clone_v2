dafa5afd04acbb6b45743486647c88e54ee9ca22
Yosinan <ymymym4634@gmial.com>
2023-04-06 14:06:17 -0700

    AirBnB_clone_v2

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Compress before sending --
+Fabric script that generates a .tgz archive from the contents of the web_static folder
+
+'''
+from time import strftime
+from datetime import datetime
+from fabric.api import local
+
+# do_pack function 
+def do_pack():
+    ''' Fabric script that generates a .tgz archive '''
+
+    arc_name = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    try:
+        local("mkdir -p version")
+        arc_path = 'versions/web_static_{}.tgz'.format(arc_name)
+        local("tar -cfzv {} web_static/".format(arc_path))
+
+        return arc_path
+    except Exception:
+        return None
