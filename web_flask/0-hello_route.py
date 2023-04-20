c7ffa6c4b86d1610f0a7579ebc59417eb71c55c2
Yosinan <ymymym4634@gmail.com>
2023-04-20 12:16:51 +0000

    Web_Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # importing flask
 from flask import Flask
 
@@ -6,4 +7,14 @@ app = Flask(__name__)
 # app decorator for routing
 @app.route('/', strict_slashes=False)
 def hello():
+    '''
+    simple route
+
+    Returns:
+        str : Hello HBNB!
+    '''
     return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
